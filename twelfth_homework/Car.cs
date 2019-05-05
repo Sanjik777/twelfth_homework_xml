ae793adc572df942253bd49ba21405cc8aa23f61
Sanjik <Beiseev_193@mail.ru>
2019-05-06 01:18:33 +0600

    v1

---
+++ twelfth_homework/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace twelfth_homework
+{
+	public class Car
+	{
+		public string Name { get; set; }
+		public string Model { get; set; }
+		public double Weight { get; set; }
+		public double Speed { get; set; }
+	}
+}
---
+++ twelfth_homework/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ Создать класс Car и описать его минимум 4-мя свойствами.
+ Создать по этому классу вручную .json и .xml файл описывающий какой-либо объект класса Car
+ */
+namespace twelfth_homework
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Car car = new Car
+			{
+				Name="Gelik",
+				Model="G500",
+				Weight=2.5,
+				Speed=200,
+			};
+		}
+	}
+}
